3baf3d4e4b37fd5237061d40600c028524d4f461
lawsonkeith <pi@raspberrypi.(none)>
2015-01-28 16:23:32 +0000

    Added dales commands

--- VoiceCtrl.sh
+++ VoiceCtrl.sh
@@ -9,45 +9,59 @@
 #
 # read input, then switch
 while true; do
-	#read switch state
-    	read val < /sys/class/gpio/gpio7/value
-	if ((val == 1)); then
-		#pressed switch, turn LED on
-        	echo 1 > /sys/class/gpio/gpio8/value
-		#get speech & truncate quotes
-		Voice=$(/home/pi/VoiceCtrl2/speech-recog.sh | tr -d '"')
-		echo $Voice
-		#ssh debug 
-		echo $Voice > /dev/shm/Recognition.txt
-		# decision time...
-		case $Voice in
-		# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        #read switch state
+        read val < /sys/class/gpio/gpio7/value
+        if ((val == 1)); then
+                #pressed switch, turn LED on
+                echo 1 > /sys/class/gpio/gpio8/value
+                #get speech & truncate quotes
+                Voice=$(/home/pi/VoiceCtrl2/speech-recog.sh | tr -d '"')
+                echo $Voice
+                #ssh debug 
+                echo $Voice > /dev/shm/Recognition.txt
+                # decision time...
+                case $Voice in
+                # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
- 			"light on" ) 	on_r1.sh
-					;;
+                        "screen on" )   on_r1.sh
+                                        ;;
 
- 			"light off" ) 	off_r1.sh
-					;;
+                        "screen off" )  off_r1.sh
+                                        ;;
 
- 			"lighting on" )	off_r1.sh
-					off_r2.sh
-					off_r3.sh
-					off_r4.sh
-					;;
+                        "good bye" )    off_r1.sh
+                                        off_r2.sh
+                                        off_r3.sh
+                                        off_r4.sh
+                                        ;;
 
- 			"lighting off" )on_r1.sh
-					on_r2.sh
-					on_r3.sh
-					on_r4.sh
-					;;
+                        "hello" )       on_r1.sh
+                                        on_r2.sh
+                                        on_r3.sh
+                                        on_r4.sh
+                                        ;;
 
-		# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-		esac
-		#LED off again
-        	echo 0 > /sys/class/gpio/gpio8/value
-	fi
+                         "monitor on" ) on_r2.sh
+                                        ;;
+
+                        "monitor off" ) off_r2.sh
+                                        ;;
+
+                        "laptop on" )   on_r3.sh
+                                        ;;
+
+                        "laptop off" )  off_r3.sh
+                                        ;;
+
+                        "light on" )    on_r4.sh
+                                        ;;
+
+                        "light off" )   off_r4.sh
+                                        ;;
+
+                # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                esac
+                #LED off again
+                echo 0 > /sys/class/gpio/gpio8/value
+        fi
 done
-
-
-
-
